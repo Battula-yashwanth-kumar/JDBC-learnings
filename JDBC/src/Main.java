cd094955a2ae260c60a0368b452d16b6b7d08a91
Yashwanth <yashwathsaikumar@gmail.com>
2024-03-19 06:44:00 +0530

    changes fixed

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
         DBFunctions db = new DBFunctions();
-       Connection con = db.connect_to_db("JDBClearning","postgres","Fuckyou@159");
+       Connection con = db.connect_to_db("JDBClearning","postgres","jhwhcjdhw");
 //        db.createTable(con,"employee");
 //        db.insertData(con,"employee","Yash","BRj fjecje");
         db.insertDynamicDataUsingPrepareStatement(con);
